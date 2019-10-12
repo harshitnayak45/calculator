c5f2d5f37f7bb658eb9a5aeae8e557dabf5d09b7
linuxbean001 <vishnulinux02@gmail.com>
2019-10-12 12:47:49 +0530

    slider update

--- main.js
+++ main.js
@@ -1,47 +1,7 @@
-
-
-let p = []
-for (let i = 10; i <=100; i++) {
-    p.push(i*1000)
-}
-console.log(p);
-let p1 = []
-for (let i = 6; i <=14; i++) {
-    p1.push(i)
-}
-
 $(document).ready(function() {
 $(".finalEmi").text("$"+0);
 $('.term1').val(0);
-
-$("#slider_amirol").slider({
-range: "min",
-animate: true,
-min: 1,
-max: 90,
-step: 1,
-slide: 
-    function(event, ui) 
-    {
-        update(1,ui.value); //changed
-     
-    }
-});
-
-$("#slider_amirol1").slider({
-range: "min",
-animate: true,
-min: 0,
-max: 8,
-step: 1,
-slide: 
-    function(event, ui) 
-    {
-        $('.term1').val(ui.value);
-        update1(1,ui.value); //changed
-     
-    }
-});
+$('.slidervalue').text(numberWithCommas("$"+10000));
 
 $(".calcualtePriceAmt").click(function(){
  calcualtePrice()
@@ -49,48 +9,30 @@ $(".calcualtePriceAmt").click(function(){
 $('.loan_rate').on('input',function(event) {
 
 var rate_val=$('.loan_rate').val();
-var step_count= parseInt(rate_val)-6;
-$('.term1').val(step_count);
-var pos=step_count*12+"%";
-if(undefined == step_count || step_count<=0){
-    step_count = 0;
-} 
- $(".fixed-int .ui-corner-all").css("left", pos);
- update1(1,step_count);
+ $('.loan_rate1').val(rate_val);
+
 });
-update();
-update1();
+$('.loan_rate1').on('input',function(event) {
+    var rate_val=$('.loan_rate1').val();
+     $('.loan_rate').val(rate_val);
+    
+    });
+    $('.sliderVal').on('input',function(event) {
+        var s_val=$('.sliderVal').val();
+         $('.slidervalue').text('$'+numberWithCommas(s_val));
+        
+        });
 
 });
 
 function numberWithCommas(x){
-// return x;
 return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function update(slider,val) {
-if(undefined == val || val<=0){
-val = 0;
-} 
-var amount = p[val];
-$('#sliderVal').val(amount);
-
-$('#slider_amirol a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> $'+numberWithCommas(amount)+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
-}
-function update1(slider,val) {
-
-if(undefined == val || val<=0){
-val = 0;
-} 
-var amount1 = p1[val];
-$('.loan_rate').val(amount1);
-$('#slider_amirol1 a').html('<label><span class="glyphicon glyphicon-chevron-left"></span>'+(amount1)+'% <span class="glyphicon glyphicon-chevron-right"></span></label>');
-}
-
 function calcualtePrice(val){
+    var sliderVal =$('.sliderVal').val();
 var term =$('.loan_term').val();
-var rate =$('.loan_rate').val();
-var sliderVal =$('#sliderVal').val();
+var rate =$('.loan_rate1').val();
 var fee=250;
 const r = rate / 12 / 100;
 const P = sliderVal
